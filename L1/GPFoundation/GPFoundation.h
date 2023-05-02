0cb9732433a1b0a79f2eb054a22180c7cba2206a
Sergey Balashov <Balashov.152@gmail.com>
2023-05-02 16:47:41 +0300

    Add layers

---
+++ L1/GPFoundation/GPFoundation.h
@@ -0,0 +1,18 @@
+//
+//  GPFoundation.h
+//  GPFoundation
+//
+//  Created by Sergey Balashov on 02.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GPFoundation.
+FOUNDATION_EXPORT double GPFoundationVersionNumber;
+
+//! Project version string for GPFoundation.
+FOUNDATION_EXPORT const unsigned char GPFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPFoundation/PublicHeader.h>
+
+
