52e5145c729233b81020f66d1a441fa427f11a1b
Sergey Balashov <Balashov.152@gmail.com>
2023-05-02 16:52:42 +0300

    Rename to GPUIComponents

---
+++ L2/GPUIComponents/GPUIComponents.h
@@ -0,0 +1,18 @@
+//
+//  GPUIComponents.h
+//  GPUIComponents
+//
+//  Created by Sergey Balashov on 02.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GPUIComponents.
+FOUNDATION_EXPORT double GPUIComponentsVersionNumber;
+
+//! Project version string for GPUIComponents.
+FOUNDATION_EXPORT const unsigned char GPUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPUIComponents/PublicHeader.h>
+
+
