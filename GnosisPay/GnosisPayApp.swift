30daf33165cafa6f2f06c9a49aa0b3853d9ef403
Sergey Balashov <Balashov.152@gmail.com>
2023-05-02 16:28:47 +0300

    Create Xcode project

---
+++ GnosisPay/GnosisPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GnosisPayApp.swift
+//  GnosisPay
+//
+//  Created by Sergey Balashov on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GnosisPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
